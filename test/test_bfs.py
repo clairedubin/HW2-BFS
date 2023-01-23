b0800ea7cc70d63b988bf23e30d4264e891949f4
Claire Dubin <clairedubin@Claires-MacBook-Pro.local>
2023-01-22 17:03:14 -0800

    added unit tests

--- test/test_bfs.py
+++ test/test_bfs.py
@@ -2,7 +2,6 @@
 import pytest
 from search import graph
 
-@pytest.fixture
 def test_bfs_traversal():
     """
     TODO: Write your unit test for a breadth-first
@@ -11,7 +10,18 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+    g = graph.Graph('data/tiny_network.adjlist')
+    marina_traverse = g.bfs("Marina Sirota")
+
+    #check that all nodes are visited once
+    assert(len(marina_traverse)==30), "All nodes should be visited once"
+    assert(len(set(marina_traverse)) == 30), "All nodes should be visited once"
+
+    #check that exception is raised for a start node not in the graph
+    with pytest.raises(Exception):
+        assert(g.bfs('Fake Node')), "Start node not in tree should raise exception"
+        
+
 
 def test_bfs():
     """
@@ -24,4 +34,19 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    pass
+
+    g = graph.Graph('data/citation_network.adjlist')
+
+    #correct example shortest path
+    correct_path = ['Nadav Ahituv', '32728249', 'Yin Shen', '33545030', 'Tony Capra'] 
+    assert g.bfs('Nadav Ahituv', 'Tony Capra') == correct_path, "Shortest path is incorrect"
+
+    #nodes that are not connected
+    assert g.bfs('34550967', '34321313') == None, "Unconnected nodes should return None"
+
+    #end node not in tree - raises exception
+    with pytest.raises(Exception):
+        assert g.bfs('34550967', 'fake end node'), "End node not in tree should raise exception"
+
+
+
